49e5abeaf942cf4eba5207fceb3d511856095271
Evan Siegel <siegel.evan@gmail.com>
2021-06-08 08:12:57 -0400

    Introduces Button Component

---
+++ components/Button/index.tsx
@@ -0,0 +1,26 @@
+import { ButtonHTMLAttributes, useRef } from "react";
+import classnames from "classnames";
+import useBackgroundPosition from "../ShinyText/useBackgroundPosition";
+import styles from "./styles.module.css";
+
+interface Props extends ButtonHTMLAttributes<HTMLButtonElement> {
+  className?: string;
+}
+
+const Button = ({ className = "", children, ...rest }: Props) => {
+  const ref = useRef(null);
+  const [x, y] = useBackgroundPosition(ref);
+
+  return (
+    <button
+      className={classnames(styles.root, className)}
+      style={{ backgroundPositionX: x, backgroundPositionY: y }}
+      ref={ref}
+      {...rest}
+    >
+      {children}
+    </button>
+  );
+};
+
+export default Button;
