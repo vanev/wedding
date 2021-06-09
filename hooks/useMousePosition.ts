26ab19a0415d1ab4fed7a2c3616269e25e46a885
Evan Siegel <siegel.evan@gmail.com>
2021-06-09 18:32:41 -0400

    Patches Memory Leak in useMousePosition

--- hooks/useMousePosition.ts
+++ hooks/useMousePosition.ts
@@ -5,15 +5,21 @@ const useMousePosition = (): Position => {
   const [position, setPosition] = useState<Position>(empty);
 
   useEffect(() => {
+    let requestId: number | null = null;
+
     const handleMouseMove = (event: MouseEvent) => {
-      window.requestAnimationFrame(() => {
+      requestId = window.requestAnimationFrame(() => {
         setPosition([event.x, event.y]);
+        requestId = null;
       });
     };
 
     window.addEventListener("mousemove", handleMouseMove, true);
 
-    return () => window.removeEventListener("mousemove", handleMouseMove, true);
+    return () => {
+      window.removeEventListener("mousemove", handleMouseMove, true);
+      if (requestId) window.cancelAnimationFrame(requestId);
+    };
   }, []);
 
   return position;
