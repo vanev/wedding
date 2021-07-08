dcb831d475b168a73c751806a1440de0106f3b84
Evan Siegel <siegel.evan@gmail.com>
2021-07-08 06:56:46 -0400

    Extract emptyDOMRect

--- hooks/useElementRect.ts
+++ hooks/useElementRect.ts
@@ -1,18 +1,19 @@
 import { RefObject } from "react";
 
+const emptyDOMRect: DOMRect = {
+  x: 0,
+  y: 0,
+  height: 0,
+  width: 0,
+  top: 0,
+  bottom: 0,
+  left: 0,
+  right: 0,
+  toJSON: () => "",
+};
+
 const useElementRect = (ref: RefObject<HTMLElement>): DOMRect => {
-  if (!ref.current)
-    return {
-      x: 0,
-      y: 0,
-      height: 0,
-      width: 0,
-      top: 0,
-      bottom: 0,
-      left: 0,
-      right: 0,
-      toJSON: () => null,
-    };
+  if (!ref.current) return emptyDOMRect;
 
   return ref.current.getBoundingClientRect();
 };
